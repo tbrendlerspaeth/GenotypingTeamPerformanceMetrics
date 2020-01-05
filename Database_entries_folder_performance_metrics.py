935a1405f23d12009c7cc40e79ac61a43016a704
tbrendlerspaeth <52102199+tbrendlerspaeth@users.noreply.github.com>
2020-01-05 12:51:16 +0000

    Add files via upload

---
+++ Database_entries_folder_performance_metrics.py
@@ -0,0 +1,106 @@
+"""
+This script is designed to collect and analyse data relevant to the work performance of Team 121's
+from the database entries folder.
+
+created by TABS
+
+version 2020.01.05
+
+"""
+
+import os
+import pandas as pd
+import datetime
+
+
+def entries_harvest():
+    entries_folder = input(r'Enter folder path of a database entries folder: ')
+
+    time_started = datetime.datetime.now()
+    print('Time started with harvesting folder activity data: ' , time_started.time())
+    df_all_entries = pd.DataFrame()
+
+    for foldername, subfolders, filenames in os.walk(entries_folder):
+        for file in filenames:
+            file_path = foldername + '\\' + file
+            try:
+                df_file = pd.read_excel(file_path)
+            except:
+                print('ALERT: Exception file: ', file)
+
+            df_all_entries = df_all_entries.append(df_file)
+    df_all_entries.dropna(axis=0, how='all', inplace=True)
+    df_all_entries.dropna(axis=1, how='all', inplace=True)
+    df_all_entries.drop_duplicates(keep='first', inplace=True)
+
+    df_transnetyx = pd.DataFrame()
+    df_t121 = pd.DataFrame()
+    for plate in df_all_entries['Plate Barcode']:
+        try:
+            if plate[0] == 'T':
+                transnetyx_plate = pd.DataFrame(df_all_entries[df_all_entries['Plate Barcode'] == plate])
+                df_transnetyx = df_transnetyx.append(transnetyx_plate, ignore_index=True)
+                df_transnetyx.drop_duplicates(keep='first', inplace=True)
+            else:
+                t121_plate = pd.DataFrame(df_all_entries[df_all_entries['Plate Barcode'] == plate])
+                df_t121 = df_t121.append(t121_plate, ignore_index=True)
+                df_t121.drop_duplicates(keep='first', inplace=True)
+        except:
+            continue
+
+    for plate in df_all_entries['Plate']:
+        try:
+            if plate[0] == 'T':
+                transnetyx_plate = pd.DataFrame(df_all_entries[df_all_entries['Plate'] == plate])
+                df_transnetyx = df_transnetyx.append(transnetyx_plate, ignore_index=True)
+                df_transnetyx.drop_duplicates(keep='first', inplace=True)
+            else:
+                t121_plate = pd.DataFrame(df_all_entries[df_all_entries['Plate'] == plate])
+                df_t121 = df_t121.append(t121_plate, ignore_index=True)
+                df_t121.drop_duplicates(keep='first', inplace=True)
+        except:
+            continue
+    # can the above for blocks and/or if statements be combined for less repetitive code?
+
+    time_finished = datetime.datetime.now()
+    print('Time finished with harvesting entries and generating dataframes: ', time_finished.time())
+    elapsed_time = time_finished - time_started
+    elapsed_time = divmod(elapsed_time.total_seconds(), 60)
+    print('Time taken for harvesting and editing: ', elapsed_time[0], 'minutes ', elapsed_time[1], 'seconds.')
+
+    total_t121_entries = len(df_t121)
+    total_transnetyx_entries = len(df_transnetyx)
+    total_entries = len(df_all_entries)
+
+    failed_t121 = len(df_t121[df_t121.Genotype.isin(['Failed', 'failed', 'failed ', ' Failed '])])
+    retest_t121 = len(df_t121[df_t121.Genotype.isin(['Retest', 'retest', 'retest ', 'Retest '])])
+    percent_failed_t121 = (failed_t121 / total_t121_entries) * 100
+    percent_retest_t121 = (retest_t121 / total_t121_entries) * 100
+
+    failed_transnetyx = len(df_transnetyx[df_transnetyx.Genotype.isin(['Failed', 'failed', 'failed ', ' Failed '])])
+    retest_transnetyx = len(df_transnetyx[df_transnetyx.Genotype.isin(['Retest', 'retest', 'retest ', 'Retest '])])
+    percent_failed_transnetyx = (failed_transnetyx / total_transnetyx_entries) * 100
+    percent_retest_transnetyx = (retest_transnetyx / total_transnetyx_entries) * 100
+
+    print('Total number of database entries: ', total_entries)
+    print('Total number of T121 entries: ', total_t121_entries)
+    print('Total number of Transnetyx entries: ', total_transnetyx_entries)
+    print('Percentage of T121 retests: ', round(percent_retest_t121, 1))
+    print('Percentage of T121 failed: ', round(percent_failed_t121, 1))
+    print('Percentage of Transnetyx retests: ', round(percent_retest_transnetyx, 1))
+    print('Percentage of Transnetyx failed: ', round(percent_failed_transnetyx, 1))
+
+    # Absolute mess. Way too repetitive. Consider learning classes to handle this better with the use of methods
+
+while True:
+    entries_harvest()
+
+"""
+Suggestions for improvement, additions, alterations:
+- Count number of different plate IDs to give some more indication of outputs for both T121 and Transnetyx
+- Highlight the most common assay names
+- Bring up the top 10 most error prone assay names for each walk.
+- ?bring up the top 10 plates with the most errors associated with them?
+- Turn the whole thing into a class? Using methods would cut down on repetition? Or maybe just use different functions?
+
+"""
