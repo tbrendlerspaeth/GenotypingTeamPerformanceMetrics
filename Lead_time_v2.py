eb83e601b446c75bf851e46bf803b5eaef91a4e9
tbrendlerspaeth <52102199+tbrendlerspaeth@users.noreply.github.com>
2020-09-07 13:17:03 +0100

    Add files via upload

--- Lead_time_v2.py
+++ Lead_time_v2.py
@@ -64,9 +64,9 @@ class LeadTime:
         pbc_data = pbc_data.loc['20200101':]
         window = window
         min_periods = min_periods
-        x = pbc_data.index
-        y = pbc_data['Lead time'].dt.days
-        plt.scatter(x, y, color='blue', s=10, label='Plate', marker='+', alpha=0.5)
+        xdata = pbc_data.index
+        ydata = pbc_data['Lead time'].dt.days
+        plt.scatter(xdata, ydata, color='blue', s=10, label='Plate', marker='+', alpha=0.5)
 
         # Create rolling statistics
         pbc_lead_times = pbc_data['Lead time'].dt.days
@@ -77,15 +77,18 @@ class LeadTime:
         # Plot rolling statistics:
         plt.plot(rolmean, color='green', label='Rolling Mean')
         plt.plot(rolucl, color='red', label='Rolling Upper Control Limit (mean+2*std)')
+        delta_time = pbc_data.index[-1] - pbc_data.index[0]
+        x1, y2 = [pbc_data.index[0], pbc_data.index[-1]], [delta_time.days, 0]
+        plt.plot(x1, y2, color='grey', linestyle='--', label='Number of days preceding most recent date')
         plt.xlabel("Date plate received", fontdict={'fontsize': 16})
         plt.ylabel("Lead time (Days)", fontdict={'fontsize': 16})
         plt.legend(loc='best', fontsize=14, facecolor='white', framealpha=1)
         plt.grid(axis='y')
-        y_max = y.max()
-        plt.yticks(np.arange(0, int(y_max + 10), 10))
+        ydata_max = ydata.max()
+        plt.yticks(np.arange(0, int(ydata_max + 10), 10))
         plt.xticks(np.arange(pbc_data.index[0], pbc_data.index[-1], datetime.timedelta(weeks=2)).astype(datetime.datetime), rotation=65)
         plt.subplots_adjust(bottom=0.2)
-        plt.title('Rolling Statistics for Lead Times, window = ' + str(window),
+        plt.title('Rolling Statistics for Plate Lead Times',
                   fontdict={'fontweight': 'bold', 'fontsize': 20})
         plt.show()
 
