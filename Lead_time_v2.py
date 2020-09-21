f0d69abc6e868d69b43d2d088af7c881f1a15398
tbrendlerspaeth <52102199+tbrendlerspaeth@users.noreply.github.com>
2020-09-21 16:42:56 +0100

    Add files via upload

--- Lead_time_v2.py
+++ Lead_time_v2.py
@@ -66,7 +66,7 @@ class LeadTime:
         min_periods = min_periods
         xdata = pbc_data.index
         ydata = pbc_data['Lead time'].dt.days
-        plt.scatter(xdata, ydata, color='blue', s=10, label='Plate', marker='+', alpha=0.5)
+        plt.scatter(xdata, ydata, color='black', s=10, label='Plate', marker='+', alpha=0.5)
 
         # Create rolling statistics
         pbc_lead_times = pbc_data['Lead time'].dt.days
@@ -77,15 +77,15 @@ class LeadTime:
         # Plot rolling statistics:
         plt.plot(rolmean, color='green', label='Rolling Mean')
         plt.plot(rolucl, color='red', label='Rolling Upper Control Limit (mean+2*std)')
-        delta_time = pbc_data['Latest upload date'][-1] - pbc_data['Latest upload date'][0]
-        x1, y2 = [pbc_data['Latest upload date'][0], pbc_data['Latest upload date'][-1]], [delta_time.days, 0]
-        plt.plot(x1, y2, color='grey', linestyle='--', label='Number of days preceding most recent upload date')
+        delta_time = pbc_data['Latest upload date'][-1] - pbc_data.index[0]
+        x_coords, y_coords = [pbc_data.index[0], pbc_data['Latest upload date'][-1]], [delta_time.days, 0]
+        plt.plot(x_coords, y_coords, color='blue', linestyle='--', alpha=0.5, label='Number of days preceding most recent upload date')
         plt.xlabel("Date plate received", fontdict={'fontsize': 16})
         plt.ylabel("Lead time (Days)", fontdict={'fontsize': 16})
         plt.legend(loc='best', fontsize=14, facecolor='white', framealpha=1)
         plt.grid(axis='y')
         ydata_max = ydata.max()
-        plt.yticks(np.arange(0, int(ydata_max + 10), 10))
+        plt.yticks(np.arange(0, int(delta_time.days + 10), 10))
         plt.xticks(np.arange(pbc_data.index[0], pbc_data.index[-1], datetime.timedelta(weeks=2)).astype(datetime.datetime), rotation=65)
         plt.subplots_adjust(bottom=0.2)
         plt.title('Rolling Statistics for Plate Lead Times',
@@ -99,6 +99,7 @@ class LeadTime:
 
 
 if __name__=="__main__":
+    print("Program running...")
     genotype_assignments = r"Z:\Genotyping\T121_metrics\Raw data\Genotype Assignments Reports\Weekly Genotype Assignments Reports"
     genotype_assignments = LeadTime(genotype_assignments = genotype_assignments)
     genotype_assignments.print_lead_time_data()
